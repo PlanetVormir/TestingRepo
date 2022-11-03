1afea5ccaed738bc732180239a2fdbd5a995ebe1
Sujal Singh <email.sujalsingh@gmail.com>
2022-11-03 18:40:26 +0530

    deployment pipeline done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (Edit: I edited it.)
+          Deployment is finally working.
         </p>
         <a
           className="App-link"
